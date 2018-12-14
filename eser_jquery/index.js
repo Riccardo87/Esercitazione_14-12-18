73eb17ff2c4d43569751e6b2d72693229e61a414
Riccardo87 <riccardo.vancini@gmail.com>
2018-12-14 13:21:55 +0100

    first commit

---
+++ eser_jquery/index.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    console.log('Il DOM è completo'); 
+  });
+
+  // Costante che contiene Array con 3 Oggetti, Ognuno contiene un ID (num) e Nome (String) //
+
+const newArray =
+[
+    {
+        id: 1,
+        name: 'Riccardo'
+    },
+
+    {
+        id: 2,
+        name: 'Lara'
+    },
+    {
+        id: 3,
+        name: 'Silvia'
+    },
+]
+
+    console.log(newArray);
+
+    /* Ciclo FOR che iteri elementi dell'array e che mi stampi ID e Name del singolo oggetto all'interno di tag HTML;
+    Prima della Stampa converire ID  da Num a Stringa
+    ID sarà contenuto da un Tag STAN e il Nome dal Tag P
+    Successivamente aggiungere una classe al tag SPAN e tramite quest'ultimo dare un Colore  #FF0000 e uno stile BOLD */
+
+    // Usare AppenChild
+
+
+    let risp = ''
+
+    for (let i=0; i < newArray.length; i++) {
+        risp += newArray[i].id + newArray[i].name
+        console.log(newArray[i]);
+    }
+
+    newArray= $('#stampArray').html();
